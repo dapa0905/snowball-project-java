096f2c6e2d3553714f82cca000309bb988c51f7c
KANGMO KOO <kookangmo0905@gmail.com>
2025-10-07 10:44:42 +0900

    20251007-firstCommit

---
+++ src/main/java/com/snowball/snowball_project_java/SnowballProjectJavaApplication.java
@@ -0,0 +1,13 @@
+package com.snowball.snowball_project_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowballProjectJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowballProjectJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snowball/snowball_project_java/SnowballProjectJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snowball.snowball_project_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowballProjectJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
